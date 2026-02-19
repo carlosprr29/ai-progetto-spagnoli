c0f6eacb51b4a8e2f849e0cc66f63474fd5326e7
antoniojoseom <antoniojose.osuna.m@gmail.com>
2026-02-19 10:33:09 +0100

    Delete notebooks/load_data.py

--- notebooks/load_data.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("davanstrien/WELFake")
-
-print(dataset["train"][0])
-dataset = load_dataset(
-    "davanstrien/WELFake",
-    cache_dir="./data"
-)
